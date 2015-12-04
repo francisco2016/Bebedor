53211d9f62d45a057b0e976466197565e1e60b82
francisco2016 <otrayama@gmail.com>
2015-12-04 20:06:40 +0100

    Añadido mt. preguntas(). las respuestas del bbdor dependen del alcohol ingerido.

--- Bebedor.java
+++ Bebedor.java
@@ -11,14 +11,17 @@ public class Bebedor
     private float alcoholEnS;
     //At para guardar el valor del límite de alcohol
     private float limite;
+    //At. para almacenar el nombre del bebedor.
+    private String nombreB;
 
     /**
      * Constructor para inicializar los atributos.
      */
-    public Bebedor(float limite)
+    public Bebedor(float limite, String nombreB)
     {
        alcoholEnS = 0f;
        this.limite = limite;
+       this.nombreB = nombreB;
        
     }
 
@@ -29,29 +32,27 @@ public class Bebedor
     {
        alcoholEnS = alcoholEnS + copa.getAlcoholEnCopa();
        if(alcoholEnS >= limite){
-           System.out.println("Otra copa??? \nNo, ya no tengo sed.  \n ");
+           System.out.println("Otra copa??? \nNo, ya se me ha quitado la sed de golpe y porrazo.  \n ");
        }
     }
+    
+    /**
+     * Mt para preguntar al bebedor y conocer su estado dependiendo del alcohol que ha tomado.
+     */
+    public void preguntas(String cadena1){
+       //alcoholEnS = alcoholEnS + copa.getAlcoholEnCopa();
+       
+       if( (alcoholEnS > limite) || (cadena1.contains(nombreB))){
+           System.out.print( "¡¡¡ " +cadena1.toUpperCase() + " ¡¡¡");
+       }
+       else if( (alcoholEnS < limite) ){
+           if( (cadena1.length() % 2 == 0)){
+               System.out.println("Si. ");
+           }
+           else{
+               System.out.println("No. ");
+           }
+       }
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
